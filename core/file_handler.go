873fe925c1f75b90db749ecd12699cea4e70d314
Taylor Monacelli <taylormonacelli@gmail.com>
2024-09-30 11:15:57 -0700

    mutex prevents us from modifying simul

--- core/file_handler.go
+++ core/file_handler.go
@@ -4,15 +4,21 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"sync"
 )
 
-type FileHandlerImpl struct{}
+type FileHandlerImpl struct {
+	mutex sync.Mutex
+}
 
 func NewFileHandler() *FileHandlerImpl {
 	return &FileHandlerImpl{}
 }
 
 func (fh *FileHandlerImpl) Read(path string) ([]string, error) {
+	fh.mutex.Lock()
+	defer fh.mutex.Unlock()
+
 	content, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
@@ -21,6 +27,9 @@ func (fh *FileHandlerImpl) Read(path string) ([]string, error) {
 }
 
 func (fh *FileHandlerImpl) Write(path string, content []string) error {
+	fh.mutex.Lock()
+	defer fh.mutex.Unlock()
+
 	file, err := os.Create(path)
 	if err != nil {
 		return err
