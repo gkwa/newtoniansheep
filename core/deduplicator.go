07383ace227c6b47abe0e2560624eb9a0471b7d5
Taylor Monacelli <taylormonacelli@gmail.com>
2024-09-30 15:28:27 -0700

    don't update file if there have been no changes

--- core/deduplicator.go
+++ core/deduplicator.go
@@ -1,6 +1,21 @@
 package core
 
-import "github.com/go-logr/logr"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strings"
+
+	"github.com/go-logr/logr"
+)
+
+type Processor interface {
+	Process(input []string) ([]string, error)
+}
+
+type FileHandler interface {
+	Read(path string) ([]string, error)
+	Write(path string, content []string) error
+}
 
 type Deduplicator struct {
 	logger      logr.Logger
@@ -27,10 +42,19 @@ func (d *Deduplicator) ProcessFile(path string) error {
 		return err
 	}
 
+	originalChecksum := d.calculateChecksum(content)
+
 	processedContent, err := d.processor.Process(content)
 	if err != nil {
 		d.logger.Error(err, "Failed to process content")
 		return err
+	}
+
+	processedChecksum := d.calculateChecksum(processedContent)
+
+	if originalChecksum == processedChecksum {
+		d.logger.V(1).Info("No changes detected, skipping file write")
+		return nil
 	}
 
 	err = d.fileHandler.Write(path, processedContent)
@@ -40,4 +64,10 @@ func (d *Deduplicator) ProcessFile(path string) error {
 	}
 
 	return nil
+}
+
+func (d *Deduplicator) calculateChecksum(content []string) string {
+	hash := sha256.New()
+	hash.Write([]byte(strings.Join(content, "\n")))
+	return hex.EncodeToString(hash.Sum(nil))
 }
